7bde0526b47fea2306d924e512a88185fae5dd41
Melissa Iman Noriega <melissa.iman.n@uni.pe>
2026-06-02 18:40:00 -0500

    chore(setup): init repo

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-config-prettier';
+
+export default tseslint.config(
+  {
+    ignores: ['dist/**', 'coverage/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+      'no-console': 'off',
+    },
+  },
+  prettier,
+);
